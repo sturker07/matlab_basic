17242851f652dbc9e097164572e9306a4d713f2c
sturker07 <sturker.07@gmail.com>
2021-04-28 11:20:36 +0300

    matematikse_operatorler
    operatör tanımlamaları yapıldı.

---
+++ degisken_tanimlama.m
@@ -0,0 +1,9 @@
+clc
+clear all
+
+
+yil = 2021;  % degisken tanimlama
+ad='SELAHATTIN' % char tanimlama
+dizi_tanimlama = [1,2;3,4] %dizi tanimlama
+whos %degiskenlerin ozelliklerini gorme
+
---
+++ matematiksel_operatorler.m
@@ -0,0 +1,11 @@
+clc
+clear all
+
+5==5  %esitlik kontrolu -1
+5==4  %esitlik kontrolu -2
+5~=5  %esit degildir kontrolu -1
+5~=4  %esit degildir kontrolu -2
+4>5   %buyuktur kontrolu -1
+6>5   %buyuktur kontrolu -2
+5>=5  %buyuktur esittir kontrolu -1
+5>=4  %buyuktur esittir kontrolu -2
