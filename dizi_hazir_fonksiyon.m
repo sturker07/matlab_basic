c6eb4b59398bb41b4bd00bb07d92bd6c276c158c
sturker07 <sturker.07@gmail.com>
2021-04-28 12:22:15 +0300

    hazir fonksiyon

--- dizi_hazir_fonksiyon.m
+++ dizi_hazir_fonksiyon.m
@@ -4,3 +4,9 @@ eye = eye(2,3) % birim matrisi
 
 length(a) % sutun sayisi
 size(a) % satir ve sutun sayisi
+
+
+% ans - yapilan islemler sonucu atanan degisken
+% pi
+% eps  - iki sayi arasýnda olabilecek en kucuk fark epsilon 
+% inf  - sayi/sifir belirsizliði 
