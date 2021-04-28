d99916f17005614ff4ec061267fad1ee250e4668
sturker07 <sturker.07@gmail.com>
2021-04-28 12:08:32 +0300

    onemli_notlar
    onemli notlar icin farklı math dosyasi acild.

---
+++ gerekli_notlar.m
@@ -0,0 +1,11 @@
+clc
+clear all
+
+% ASCII donusturme 
+double('M')
+
+% artis miktari duzenli diziler
+A= linspace(0,10,6) % 0 ile 10 arasinda düzenli 6 tane sayi olusturur.
+
+% logaritmik olarak artan dizi
+B = logspace(0,2,3)
